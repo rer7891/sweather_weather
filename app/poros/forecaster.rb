01b546da5639da3df16ab5986d730c7aa4a8f88a
Becky Robran <rerobran@gmail.com>
2020-02-29 20:32:53 -0700

    adds api call for forecaster

--- app/poros/forecaster.rb
+++ app/poros/forecaster.rb
@@ -6,13 +6,15 @@ class Forecaster
   end
 
   def get_coordinates
-    Google.new(@location).get_coordinates
+    location_details = GoogleService.new.geocode(@location)
+    coordinates = location_details[:results].first[:geometry][:location]
+    lat = coordinates[:lat]
+    lng = coordinates[:lng]
+    GoogleLocation.new(lat, lng)
   end
 
   def get_weather
-    WeatherForecast.new(get_coordinates)
+    forecast = DarkskyService.new.get_weather(get_coordinates)
+    WeatherForecast.new(forecast)
   end
-
-
-
 end
