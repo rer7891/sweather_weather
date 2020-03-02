d17c8267ef780f9bddacded171ac02d46e7f5803
Becky Robran <rerobran@gmail.com>
2020-03-02 10:50:24 -0700

    gets service to work

--- app/services/yelp_service.rb
+++ app/services/yelp_service.rb
@@ -1,14 +1,16 @@
 class YelpService
   def get_restaurant(travel_data, food)
-    time = Time.now.to_i + travel_data.travel_time
-    url = "v3/businesses/search?location=#{travel_data.destination_name}&limit=1&term=#{food}&open_at=#{time}"
+    search_params = food + " " + "food"
+    time = Time.now.to_i + travel_data.format_travel_time
+    url = "v3/businesses/search?location=#{travel_data.format_destination}&limit=1&term=#{search_params}&open_at=#{time}"
+    get_json(url)
   end
 
   def get_json(url)
-    key = EVN['YELP_API_KEY']
-    conn = Faraday.new(url: "https://api.yelp.com/#{url}") do |faraday|
+    key = ENV['YELP_API_KEY']
+    response = Faraday.get("https://api.yelp.com/#{url}") do |faraday|
       faraday.headers["Authorization"] = "Bearer #{key}"
-      faraday.adapter Faraday.default_adapter
     end
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
