6a9ea5d6778f4c45d88c9d832ea0f6fbbba627c9
Becky Robran <rerobran@gmail.com>
2020-03-02 11:39:22 -0700

    cassettes

--- spec/requests/api/v1/yelp/get_yelp_restaurant_spec.rb
+++ spec/requests/api/v1/yelp/get_yelp_restaurant_spec.rb
@@ -7,7 +7,12 @@ describe "can return a restaurant in a certain city" do
 
       get '/api/v1/munchies?start=denver,co&end=pueblo,co&food=chinese'
       expect(response).to have_http_status(:success)
-      forecast = JSON.parse(response.body)
+      travel_details = JSON.parse(response.body)
+
+      expect(travel_details["data"]["attributes"]["end_location"]).to eq("Pueblo, CO, USA")
+      expect(travel_details["data"]["attributes"]["travel_time"]).to eq("1 hour 48 mins")
+      expect(travel_details["data"]["attributes"]["forecast"]["summary"]).to eq("Clear")
+      expect(travel_details["data"]["attributes"]["restaurant"]["name"]).to eq("Kan's Kitchen")
     end
   end
 end
