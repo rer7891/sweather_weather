b9f626a10e2d90f7c87b26da9a5de9d33d207f49
Becky Robran <rerobran@gmail.com>
2020-03-02 11:38:55 -0700

    adds attr reader and id to travel details

--- app/poros/travel_details.rb
+++ app/poros/travel_details.rb
@@ -1,12 +1,18 @@
 class TravelDetails
+  attr_reader :id, :end_location, :travel_time, :forecast
+
   def initialize(travel, forecast, restaurant)
+    @id = nil
     @end_location = travel.destination_name
     @travel_time = travel.travel_time
-    @forecast = forecast.forecast_hourly[10]
+    @forecast = forecast.forecast_hourly[13]
     @restaurant = restaurant
   end
 
   def restaurant
-    @restaurant
+    restaurant = {}
+    restaurant[:name] = @restaurant.name
+    restaurant[:addres] = [@restaurant.address, @restaurant.city, @restaurant.zip, @restaurant.state]
+    restaurant
   end
 end
