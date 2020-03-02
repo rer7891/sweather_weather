7da962ae3d35fb7aeab1cb9cf680cb5b8cde74d3
Becky Robran <rerobran@gmail.com>
2020-03-01 18:43:04 -0700

    adds image test

---
+++ spec/requests/api/v1/images/back_ground_image_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+require 'rails_helper'
+
+describe "can return a background image" do
+
+  describe "GET images#show", :vcr do
+    it "sends a background image for specific location" do
+
+      get "/api/v1/backgrounds?location=denver,co"
+      expect(response).to have_http_status(:success)
+      image = JSON.parse(response.body)
+    end
+  end
+end
