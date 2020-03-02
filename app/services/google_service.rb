69e38da52baa5384696298bc0ebc0eb7699ae9c2
Becky Robran <rerobran@gmail.com>
2020-03-02 09:36:57 -0700

    adds new url to google service

--- app/services/google_service.rb
+++ app/services/google_service.rb
@@ -1,15 +1,21 @@
 class GoogleService
 
+  def get_travel_time(start_loc, end_loc)
+    key = ENV['GOOGLE_API_KEY']
+    url = "directions/json?origin=#{start_loc}&destination=#{end_loc}&key=#{key}"
+    get_json(url)
+  end
+
   def geocode(location)
     key = ENV['GOOGLE_API_KEY']
-    url = "maps/api/geocode/json?address=#{location}&key=#{key}"
+    url = "geocode/json?address=#{location}&key=#{key}"
     get_json(url)
   end
 
 private
 
   def get_json(url)
-    response = Faraday.get("https://maps.googleapis.com/#{url}")
+    response = Faraday.get("https://maps.googleapis.com/maps/api/#{url}")
     JSON.parse(response.body, symbolize_names: true)
   end
 end
