ec08a4e98778ca9801d7aedd0728a4a4e5b613a9
Becky Robran <rerobran@gmail.com>
2020-03-02 11:58:34 -0700

    updates test due to new hash

--- spec/requests/api/v1/yelp/get_yelp_restaurant_spec.rb
+++ spec/requests/api/v1/yelp/get_yelp_restaurant_spec.rb
@@ -11,7 +11,7 @@ describe "can return a restaurant in a certain city" do
 
       expect(travel_details["data"]["attributes"]["end_location"]).to eq("Pueblo, CO, USA")
       expect(travel_details["data"]["attributes"]["travel_time"]).to eq("1 hour 48 mins")
-      expect(travel_details["data"]["attributes"]["forecast"]["summary"]).to eq("Clear")
+      expect(travel_details["data"]["attributes"]["forecast"]).to eq("Clear")
       expect(travel_details["data"]["attributes"]["restaurant"]["name"]).to eq("Kan's Kitchen")
     end
   end
