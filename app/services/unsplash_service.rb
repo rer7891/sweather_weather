da1e45459df34e28fb572571a2297b2d12fa0936
Becky Robran <rerobran@gmail.com>
2020-03-01 18:41:25 -0700

    adds image service

---
+++ app/services/unsplash_service.rb
@@ -0,0 +1,14 @@
+class UnsplashService
+  def get_image_json(location)
+    key = ENV['UNSPLASH_API_KEY']
+    url = "search/photos?query=#{location[0]}&query=#{location[1]}&per_page=1&client_id=#{key}"
+    get_json(url)
+  end
+
+private
+
+  def get_json(url)
+    response = Faraday.get("https://api.unsplash.com/#{url}")
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
