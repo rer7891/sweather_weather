9075a14f995b4f9244f1431138af5e62ea54972b
Becky Robran <rerobran@gmail.com>
2020-03-02 11:26:07 -0700

    updates yelp details

---
+++ app/poros/yelp_details.rb
@@ -0,0 +1,11 @@
+class YelpDetails
+  attr_reader :name, :address, :city, :zip, :state
+
+  def initialize(data)
+    @name = data[:businesses][0][:name]
+    @address = data[:businesses][0][:location][:address1]
+    @city = data[:businesses][0][:location][:city]
+    @zip = data[:businesses][0][:location][:zip]
+    @state = data[:businesses][0][:location][:state]
+  end
+end
