7f8fd9a60eb5efdcd7b277a142662f91ad9a30a2
Becky Robran <rerobran@gmail.com>
2020-03-01 18:35:58 -0700

    adds location details to google location

--- app/poros/google_location.rb
+++ app/poros/google_location.rb
@@ -1,7 +1,8 @@
 class GoogleLocation
-  attr_reader :lat, :lng
-  def initialize(lat, lng)
-    @lat = lat
-    @lng = lng
+  attr_reader :lat, :lng, :location
+  def initialize(coordinates)
+    @location = coordinates[0][:formatted_address]
+    @lat = coordinates[0][:geometry][:location][:lat]
+    @lng = coordinates[0][:geometry][:location][:lng]
   end
 end
