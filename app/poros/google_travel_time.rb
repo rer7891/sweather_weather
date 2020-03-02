63ccf80adc511907b6730a461de8814ddea3390c
Becky Robran <rerobran@gmail.com>
2020-03-02 10:50:14 -0700

    formats variables for url insertion

--- app/poros/google_travel_time.rb
+++ app/poros/google_travel_time.rb
@@ -7,4 +7,20 @@ class GoogleTravelTime
     @destination_name = data[:routes][0][:legs][0][:end_address]
     @origin_name = data[:routes][0][:legs][0][:start_address]
   end
+
+  def format_travel_time
+    time = @travel_time.split(" ")
+    hour_in_seconds = (((time[0].to_i) *60) * 60)
+    min_in_seconds = ((time[2].to_i) * 60)
+    hour_in_seconds + min_in_seconds
+  end
+
+  def format_destination
+      name = @destination_name.split(" ")
+      destination_name = name.delete_at(2)
+      co = name[1].split(",")
+      new_name = name.delete_at(1)
+      name << co
+      name.join.downcase
+  end
 end
