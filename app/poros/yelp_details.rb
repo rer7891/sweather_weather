2a9d330be2467b0b0dc49073fe850c048ad844a2
Becky Robran <rerobran@gmail.com>
2020-03-02 11:39:03 -0700

    updates zip hash

--- app/poros/yelp_details.rb
+++ app/poros/yelp_details.rb
@@ -5,7 +5,7 @@ class YelpDetails
     @name = data[:businesses][0][:name]
     @address = data[:businesses][0][:location][:address1]
     @city = data[:businesses][0][:location][:city]
-    @zip = data[:businesses][0][:location][:zip]
+    @zip = data[:businesses][0][:location][:zip_code]
     @state = data[:businesses][0][:location][:state]
   end
 end
