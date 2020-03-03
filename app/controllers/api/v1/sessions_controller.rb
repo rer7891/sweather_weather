c173a5319864e2d4cd0c117c24a00d103f710070
Becky Robran <rerobran@gmail.com>
2020-03-02 21:11:13 -0700

    adds sessions controller

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::SessionsController < ApplicationController
+  def create
+    user = User.find_by(email: params[:email])
+    if login_successful?(user)
+      session[:user_id] = user.id
+      render json: {body: {auth_token: user.auth_token}, status: "Success 200."}
+    else
+      render json: {description: "Login Failed. Bad credentials.", response: "Error 404"}
+    end
+  end
+
+  private
+
+  def login_successful?(user)
+    user && user.authenticate(params[:password])
+  end
+end
