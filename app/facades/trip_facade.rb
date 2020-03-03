2f3e58b04ecee4ab162644c2e1a7e342a725168d
Becky Robran <rerobran@gmail.com>
2020-03-03 13:53:49 -0700

    adds travel methods to facade

--- app/facades/trip_facade.rb
+++ app/facades/trip_facade.rb
@@ -4,5 +4,13 @@ class TripFacade
     @destination = trip_details[:destination]
   end
 
-  
+  def travel_time
+    service = GoogleService.new
+    service.get_travel_time(@start, @destination)
+  end
+
+  def travel_details
+    TravelDetails.new(travel_time)
+  end
+
 end
