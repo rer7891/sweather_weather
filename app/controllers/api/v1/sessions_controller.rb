ee9a61694dd5113ec242b7e4c858a450bde75288
Becky Robran <rerobran@gmail.com>
2020-03-03 10:29:45 -0700

    Updates sessions controller responses

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,10 +2,11 @@ class Api::V1::SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     if login_successful?(user)
-      session[:user_id] = user.id
-      render json: body: {auth_token: user.auth_token}, status: :created
+      response.status = 200
+      render json: {auth_token: user.auth_token}
     else
-      render json: body: {description: "Login Failed. Bad credentials."}, status: :unauthorized
+      response.status = 401
+      render json: {description: "Login Failed. Bad credentials."}
     end
   end
 
