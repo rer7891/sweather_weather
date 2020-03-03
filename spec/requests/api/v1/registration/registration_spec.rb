a8eda2871d9042d1fd11420c0b673f01c1bec7b4
Becky Robran <rerobran@gmail.com>
2020-03-02 21:12:42 -0700

    adds registration spec

--- spec/requests/api/v1/registration/registration_spec.rb
+++ spec/requests/api/v1/registration/registration_spec.rb
@@ -6,9 +6,22 @@ describe "can return a registered user" do
     it "can create a new user" do
 
       params = {
-                  email: "whatever@example.com",
-                  password: "password",
-                  password_confirmation: "password"
+                  email: "sogood@example.com",
+                  password: "42652452",
+                  password_confirmation: "42652452"
+                }
+      post '/api/v1/users', params: params
+
+      expect(response).to have_http_status(:success)
+      new_user_response = JSON.parse(response.body)
+    end
+
+    it "can get error messages" do
+
+      params = {
+                  email: "sogood@example.com",
+                  password: "426523432",
+                  password_confirmation: "42652452"
                 }
       post '/api/v1/users', params: params
 
