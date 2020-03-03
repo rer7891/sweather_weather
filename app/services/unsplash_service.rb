1d71eefdc5e6e635f4639a85ffdcfe6324595505
Becky Robran <rerobran@gmail.com>
2020-03-03 13:24:46 -0700

    updates params to make a better image return

--- app/services/unsplash_service.rb
+++ app/services/unsplash_service.rb
@@ -1,7 +1,7 @@
 class UnsplashService
   def get_image_json(location)
     key = ENV['UNSPLASH_API_KEY']
-    url = "search/photos?query=#{location[0]}&query=#{location[1]}&per_page=1&client_id=#{key}"
+    url = "search/photos?query=#{location[0]}&per_page=1&client_id=#{key}"
     get_json(url)
   end
 
