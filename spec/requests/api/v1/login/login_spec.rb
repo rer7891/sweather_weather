5fc8a04e545498ab0658cd2df68ef38f16af4dcb
Becky Robran <rerobran@gmail.com>
2020-03-02 21:12:22 -0700

    adds login spec

---
+++ spec/requests/api/v1/login/login_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+describe "can return a user" do
+
+  describe "POST #create", :vcr do
+    before :each do
+      params = {
+                  email: "sogood@example.com",
+                  password: "42652452",
+                  password_confirmation: "42652452"
+                }
+      post '/api/v1/users', params: params
+      @user = User.last
+    end
+
+    it "can create a session for a user" do
+
+      params = {
+                  email: "sogood@example.com",
+                  password: "42652452"
+                }
+
+      post '/api/v1/sessions', params: params
+
+      expect(response).to have_http_status(:success)
+      user_response = JSON.parse(response.body)
+    end
+
+    it "can get error messages" do
+
+      params = {
+                  email: "sogood@example.com",
+                  password: "42634523432"
+                }
+      post '/api/v1/users', params: params
+
+      expect(response).to have_http_status(:success)
+      new_user_response = JSON.parse(response.body)
+    end
+  end
+end
