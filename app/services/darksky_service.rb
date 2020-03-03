31989fcbf3fac11adf82ea4be3e3408e646e09ce
Becky Robran <rerobran@gmail.com>
2020-03-03 13:54:57 -0700

    adds url to dark sky service

--- app/services/darksky_service.rb
+++ app/services/darksky_service.rb
@@ -1,4 +1,10 @@
 class DarkskyService
+  def get_weather_forecast(travel_time)
+    key = ENV['DARK_SKY_API_KEY']
+    time = Time.now.to_i + travel_time.format_travel_time
+    url = "forecast/#{key}/#{travel_time.lat},#{travel_time.lng},#{time}"
+    get_json(url)
+  end
 
   def get_weather(coordinates)
     key = ENV['DARK_SKY_API_KEY']
