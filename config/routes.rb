ed2417da031bc97b42a9fcd750f999903288e3ab
Becky Robran <rerobran@gmail.com>
2020-03-02 17:05:25 -0700

    adds route to register a user

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get 'forecast', to: 'forecasts#index'
       get 'backgrounds', to: 'images#show'
+      post 'users', to: 'registration#create'
     end
   end
 end
