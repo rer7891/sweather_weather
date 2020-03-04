a0d87cbc98b3dd7179bcf9883eece149043c0f92
Becky Robran <rerobran@gmail.com>
2020-03-03 17:04:45 -0700

    updates facade for serializer

--- app/facades/trip_facade.rb
+++ app/facades/trip_facade.rb
@@ -1,16 +1,31 @@
 class TripFacade
+  attr_reader :id, :destination, :origin
   def initialize(trip_details)
-    @start = trip_details[:origin]
-    @destination = trip_details[:destination]
+    @id = nil
+    @origin = trip_details[:origin].downcase
+    @destination = trip_details[:destination].downcase
   end
 
   def travel_time
     service = GoogleService.new
-    service.get_travel_time(@start, @destination)
+    service.get_travel_time(@origin, @destination)
   end
 
   def travel_details
-    TravelDetails.new(travel_time)
+    @trip ||= TravelDetails.new(travel_time[:routes])
   end
 
+  def get_trip_weather
+    service = DarkskyService.new
+    service.get_weather_forecast(travel_details)
+  end
+
+  def trip_forecast
+    weather = WeatherForecast.new(get_trip_weather, @destination)
+    weather.trip_forecast
+  end
+
+  def trip_duration
+    travel_details.trip_time
+  end
 end
