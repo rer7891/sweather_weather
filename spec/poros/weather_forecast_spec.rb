137b6063f69fbd7a14b3213035ed52526e059bb3
Becky Robran <rerobran@gmail.com>
2020-03-03 19:27:28 -0700

    adds poro testing

--- spec/poros/weather_forecast_spec.rb
+++ spec/poros/weather_forecast_spec.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
 
-describe "Weather Forecaster" do
+describe "Weather Forecaster", :vcr do
+  before :each do
+    forecaster = Forecaster.new("denver,co")
+    forecast = forecaster.get_weather
+    @weather = WeatherForecast.new(forecast, "denver,co")
+  end
+
   it "can initialize with details" do
+    expect(@weather.forecast_currently[:time]).to_not eq(nil)
+    expect(@weather.forecast_daily.first[:summary]).to_not eq(nil)
+    expect(@weather.forecast_hourly.first[:summary]).to_not eq(nil)
   end
 end
