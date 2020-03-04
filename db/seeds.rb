bc2ec83dfe8e9e85b3585261bc5231ad51f09270
Becky Robran <rerobran@gmail.com>
2020-03-03 18:19:47 -0700

    adds user to heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+@user = User.create(email: "sogood@gmail.com", password: "1234", password_confirmation: "1234")
