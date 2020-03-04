7a6172d3b7e52a39f7f77b16bcd27cdc502f0d3f
Becky Robran <rerobran@gmail.com>
2020-03-03 17:06:42 -0700

    adds test for planning a trip

--- spec/requests/road_trip/can_plan_a_road_trip_spec.rb
+++ spec/requests/road_trip/can_plan_a_road_trip_spec.rb
@@ -20,6 +20,24 @@ describe "can return weather forcasts and trip details" do
 
       expect(response).to have_http_status(:success)
       trip = JSON.parse(response.body)
+      expect(trip["data"]["attributes"]["destination"]).to eq("pueblo,co")
+      expect(trip["data"]["attributes"]["origin"]).to eq("denver,co")
+      expect(trip["data"]["attributes"]["trip_duration"]).to eq("1 hour 48 mins")
+    end
+    it "sends an error if a user is not registered" do
+      allow(Time).to receive(:now).and_return(Time.parse('2020-03-02 11:22:54 -0700'))
+
+      params = {
+                  "origin": "Denver,CO",
+                  "destination": "Pueblo,CO",
+                  "api_key": "52464151452"
+                }
+
+      post '/api/v1/road_trip', params: params
+
+      expect(response).to have_http_status()
+      error = JSON.parse(response.body)
+
     end
   end
 end
